b19d9030ce687d83c704eed5d23dea1cc392a7e0
rohitarora <rohitarora15820@gmail.com>
2026-01-01 16:46:52 +0530

    utils code

--- src/db/index.js
+++ src/db/index.js
@@ -1,15 +1,29 @@
+// import mongoose from "mongoose";
+// import { DB_NAME } from "../constants.js";
+
+// const connectDB = async()=>{
+//     try{
+//       const connectionInstance = await mongoose.connect
+//       (`${process.env.MONGODB_URI}/${DB_NAME}`)
+//       console.log(`MongoDB Connected !! DB HOST:$
+//         {connectionInstance.connection.host}`);
+//     }catch(error){
+//     console.log("mongoDB connection error",error);
+//     process.exit(1);
+//     }
+// }
+// export default connectDB;
+
 import mongoose from "mongoose";
-import { DB_NAME } from "../constants.js";
+import {DB_NAME} from "../constants.js";
 
 const connectDB = async()=>{
-    try{
-      const connectionInstance = await mongoose.connect
-      (`${process.env.MONGODB_URI}/${DB_NAME}`)
-      console.log(`MongoDB Connected !! DB HOST:$
-        {connectionInstance.connection.host}`);
-    }catch(error){
-    console.log("mongoDB connection error",error);
+  try{
+   const connectionInstance= await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+   console.log(`MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
+  }catch(error){
+    console.log("mongodb connection error",error);
     process.exit(1);
-    }
+  }
 }
 export default connectDB;
